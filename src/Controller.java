a2670ff19b9eef60e58c6ae76c366035f99bbdd2
Samuel Glogger <smorfi98@gmail.com>
2016-06-10 11:25:09 +0200

    Make first GUI and give Controller main Method.

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,14 @@
+import javafx.application.Application;
+
 /**
  * Created by michael.ioschpa on 08.06.2016.
  */
-public class Controller {
+public class Controller{
+     static Questions questions;
+     static GUI gui = new GUI();
+
+    public static void main(String[] args) {
+        gui.startGUI(args);
+    }
+
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,50 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 /**
  * Created by michael.ioschpa on 08.06.2016.
  */
-public class GUI {
+public class GUI extends Application{
+
+    public void startGUI(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage){
+
+        primaryStage.setTitle("QuizGame");
+        primaryStage.show();
+        VBox startMenu = new VBox();
+        Button start = new Button("Start");
+        Button quit = new Button("Quit");
+        Button settings = new Button("Settings");
+        ArrayList<Button> buttons = new ArrayList<>();
+        buttons.add(start);
+        buttons.add(settings);
+        buttons.add(quit);
+        for (Button button : buttons){
+            button.setMinSize(50,30);
+        }
+
+        Scene scene = new Scene(startMenu,400,300);
+
+        scene.setFill(Color.AQUA);
+        startMenu.setSpacing(20);
+        startMenu.setAlignment(Pos.CENTER);
+        startMenu.getChildren().addAll(start,settings,quit);
+
+
+        primaryStage.setScene(scene);
+
+
+    }
 }
