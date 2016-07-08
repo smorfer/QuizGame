1a49ad794a74e3ad25c38048f7c9144d2d6bb40f
Sebastian Rink <Sebastian Rink>
2016-07-08 10:32:05 +0200

    Implemented Question

--- src/com/failexe/Questions/Question.java
+++ src/com/failexe/Questions/Question.java
@@ -4,4 +4,68 @@ package com.failexe.Questions;
  * Created by sebastian.rink on 06.07.2016.
  */
 public class Question {
+
+    private String question;
+    private String trueAnswer;
+    private String falseAnswer1;
+    private String falseAnswer2;
+    private String falseAnswer3;
+    private double weighting;
+
+    public Question (String q, String ta, String fa1, String fa2, String fa3, double w){
+        question = q;
+        trueAnswer = ta;
+        falseAnswer1 = fa1;
+        falseAnswer2 = fa2;
+        falseAnswer3 = fa3;
+        weighting = w;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public void setTrueAnswer(String trueAnswer) {
+        this.trueAnswer = trueAnswer;
+    }
+
+    public String getFalseAnswer1() {
+        return falseAnswer1;
+    }
+
+    public void setFalseAnswer1(String falseAnswer1) {
+        this.falseAnswer1 = falseAnswer1;
+    }
+
+    public String getFalseAnswer2() {
+        return falseAnswer2;
+    }
+
+    public void setFalseAnswer2(String falseAnswer2) {
+        this.falseAnswer2 = falseAnswer2;
+    }
+
+    public String getFalseAnswer3() {
+        return falseAnswer3;
+    }
+
+    public void setFalseAnswer3(String falseAnswer3) {
+        this.falseAnswer3 = falseAnswer3;
+    }
+
+    public double getWeighting() {
+        return weighting;
+    }
+
+    public void setWeighting(double weighting) {
+        this.weighting = weighting;
+    }
 }
