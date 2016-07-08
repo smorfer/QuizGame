c691283fb61d22af830c3ff760d3d0bb3fabc1cd
Sebastian Rink <Sebastian Rink>
2016-07-08 22:48:02 +0200

    Try to Implement readQuestions in ClassQuestions.

--- src/com/failexe/Questions/Questions.java
+++ src/com/failexe/Questions/Questions.java
@@ -1,5 +1,6 @@
 package com.failexe.Questions;
 import com.failexe.List.Listelement;
+import java.io.*;
 
 public class Questions implements QuestionsInterface {
 
@@ -13,6 +14,18 @@ public class Questions implements QuestionsInterface {
         return first;
     }
 
+    private static void readQuestions() throws  IOException {
+        FileReader fr = new FileReader("Fragen.docx");
+        BufferedReader br = new BufferedReader(fr);
+
+        String q = br.readLine();
+        String ta = br.readLine();
+        String fa1 = br.readLine();
+        String fa2 = br.readLine();
+        String fa3 = br.readLine();
+        double w = br.readLine();
+        new Question(q,ta,fa1,fa2,fa3,w);
+    }
 
     @Override
 
