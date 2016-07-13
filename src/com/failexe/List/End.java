cc738f347a1820eeafa3b0d7ee7d7c6e26fac5ef
xmichi29 <xmichi29@gmail.com>
2016-07-13 09:13:59 +0200

    add Node methods

--- src/com/failexe/List/End.java
+++ src/com/failexe/List/End.java
@@ -24,7 +24,7 @@ public class End extends Listelement {
     {}
 
     public int getNodeQuantity()
-    {}
+    {return 0;}
 
     public void showData()
     {}
--- src/com/failexe/List/Node.java
+++ src/com/failexe/List/Node.java
@@ -4,4 +4,36 @@ package com.failexe.List;
  * Created by sebastian.rink on 08.07.2016.
  */
 public class Node {
+    private Listelement next;
+    private Content content;
+
+    public Node(Listelement n, Content c)
+    {
+        next = n;
+        content = c;
+    }
+
+    public void setNext(Listelement n)
+    {next=n;}
+
+    public void setContent(Content c)
+    {content = c;}
+
+    public Listelement getNext()
+    {return next;}
+
+    public Content getContent()
+    {return content;}
+
+    public Node addNode()
+    {}
+
+    public Listelement delete()
+    {}
+
+    public int getNodeQuantity()
+    {return 1+next.getNodeQuantity();}
+
+    public void showData()
+    {}
 }
