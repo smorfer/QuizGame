ef9aa9b7f2655016c1d7dfc5146b12ef487ecc8b
flogi2016 <sarina.dzurokovic@gmail.com>
2016-07-15 10:30:26 +0200

    Highscore

--- src/com/failexe/Highscore/score.java
+++ src/com/failexe/Highscore/score.java
@@ -5,5 +5,33 @@ package com.failexe.Highscore;
  */
 
 public class Score {
+    private int score;
+    private HighScore highScore;
 
+    public Score (HighScore highScore) {
+        score=0;
+        this.highScore = highScore;
+    }
+
+    public void increaseScore(){
+        score ++;
+    }
+
+    public void decreaseScore(){
+        if(score > 0){
+            score--;
+        }
+    }
+
+    public void resetScore(){
+        score=0;
+    }
+
+    public int getScore(int score){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score =score;
+    }
 }
