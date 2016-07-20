cfc2cbb41e18dfcb50f57e10c31231fa95a597b0
smorfer <smorfi98@gmail.com>
2016-07-20 20:15:01 +0200

    Made the file paths relative.

--- src/com/failexe/Questions/Questions.java
+++ src/com/failexe/Questions/Questions.java
@@ -14,16 +14,16 @@ public class Questions implements QuestionsInterface {
         /*for(int i = 1; i < 10; i++){
             this.files[i] = new File("files/Questions" + String.valueOf(1) + ".txt");
         }*/
-        this.files[0] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions1.txt");
-        this.files[1] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions2.txt");
-        this.files[2] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions3.txt");
-        this.files[3] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions4.txt");
-        this.files[4] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions5.txt");
-        this.files[5] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions6.txt");
-        this.files[6] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions7.txt");
-        this.files[7] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions8.txt");
-        this.files[8] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions9.txt");
-        this.files[9] = new File("D:\\Projects\\Java\\Java 8\\QuizGame\\src\\com\\failexe\\Questions\\files\\Questions10.txt");
+        this.files[0] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions1.txt");
+        this.files[1] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions2.txt");
+        this.files[2] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions3.txt");
+        this.files[3] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions4.txt");
+        this.files[4] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions5.txt");
+        this.files[5] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions6.txt");
+        this.files[6] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions7.txt");
+        this.files[7] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions8.txt");
+        this.files[8] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions9.txt");
+        this.files[9] = new File(".\\src\\com\\failexe\\Questions\\files\\Questions10.txt");
 
         for(int i = 0; i< 10; i++){
             this.lists[i] = getQuestions(i);
